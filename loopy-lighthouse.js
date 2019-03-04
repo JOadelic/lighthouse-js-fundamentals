42b4c532aa6e96b05ce77602ab88deb52ec57a25
Jordan <jordanowens86@gmail.com>
2019-03-04 12:01:30 +0000

    counting for loop - stage 1

---
+++ loopy-lighthouse.js
@@ -0,0 +1,10 @@
+function loopyLighthouse () {
+
+  // for loop counts 100-200
+  var counter = [];
+  for (var i = 100; i <= 200; i++) {
+    counter.push(i)
+  } return counter;
+}
+
+console.log(loopyLighthouse());
